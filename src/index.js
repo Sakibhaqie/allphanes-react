5f9f5a69300d8cb1019d42a9261610ea8b98956b
sakibhaqie <imdadulhaque72@gmail.com>
2022-03-22 21:19:18 +0530

    noob

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 // css 
 import './assets/css/pages/auth.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -8,10 +8,10 @@ import './assets/css/pages/profile.css'
 import './assets/css/navbar.css'
 // pages 
 import Layout from "./views/pages/Layout"
-import Profile from './views/pages/Profile';
-import Registration from './views/pages/Registration';
-import OtpVerification from './views/pages/OtpVerification';
-import Login from './views/pages/Login';
+import Profile from './views/pages/Profile'
+import Registration from './views/pages/Registration'
+import OtpVerification from './views/pages/OtpVerification'
+import Login from './views/pages/Login'
 
 ReactDOM.render(
   <Router>
@@ -26,7 +26,7 @@ ReactDOM.render(
     </Routes>
   </Router>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
